518648574b32f55be53693b804a489c632db5766
Ron Fox <fox@nscl.msu.edu>
2006-03-14 15:37:07 +0000

    This commit was manufactured by cvs2svn to create tag 'nscldaq-8-0-007'.

--- clients/HPProductionReadout/CDocumentedPacket.h
+++ clients/HPProductionReadout/CDocumentedPacket.h
@@ -310,6 +310,15 @@ using namespace std;
    Modification History:
    
      $Log$
+     Revision 1.1.2.2  2005/07/14 20:23:33  ron-fox
+     Correct ushort* documented packet first crack.
+
+     Revision 1.1.2.1  2005/07/14 20:18:27  ron-fox
+     Properly port this to hp style readout.. with ushort* not ushort.
+
+     Revision 8.2  2005/06/24 11:30:50  ron-fox
+     Bring the entire world onto the 8.2 line
+
      Revision 1.1  2005/02/07 19:50:46  ron-fox
      Break off branch for HPProduction readout (using transparent copyin).
 
@@ -341,7 +350,7 @@ private:
   STD(string) m_sDescription;	//!< Long description of tag.
   STD(string) m_sVersion;		//!< Version of the packet.
   STD(string) m_sInstantiationDate;	//!< Date/time object instantiated.
-  DAQWordBufferPtr m_pHeaderPtr; //!< 'pointer' to header of current packet.
+  unsigned short* m_pHeaderPtr; //!< 'pointer' to header of current packet.
   bool m_fPacketInProgress;	//!< true if packet being built now.
  
 public:
@@ -397,7 +406,7 @@ public:
     return m_sInstantiationDate;
   }
   
-  DAQWordBufferPtr getHeaderPtr() const; // Throws if pkt not open.
+  unsigned short* getHeaderPtr() const; // Throws if pkt not open.
  
   bool getPacketInProgress() const {
     return m_fPacketInProgress;
@@ -406,8 +415,8 @@ public:
   // Class operations:
   
   STD(string) Format ()  ;
-  DAQWordBufferPtr Begin (DAQWordBufferPtr& rPointer)  ;
-  DAQWordBufferPtr End (DAQWordBufferPtr& rBuffer)  ;
+  unsigned short* Begin (unsigned short* rPointer)  ;
+  unsigned short* End (unsigned short* rBuffer)  ;
   
 };
 
