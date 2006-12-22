56c5e56629662e53df3839cf04060ce78cb9520f
Ron Fox <fox@nscl.msu.edu>
2006-12-22 14:29:28 +0000

    This commit was manufactured by cvs2svn to create tag 'daqclients_8_0_009'.

--- clients/bcnaf/bcnaf.tcl
+++ clients/bcnaf/bcnaf.tcl
@@ -5,8 +5,8 @@ exec tclsh ${0} ${@}
 set me  [info script]
 set dir [file dirname $me]
 
-set cesbcnaf    [file join $dir cesbcnaf.tcl]
-set wienerbcnaf [file join $dir wienerbcnaf.tcl]
+set cesbcnaf    [file join $dir cesbcnaf]
+set wienerbcnaf [file join $dir wienerbcnaf]
 
 
 proc usage {} {
@@ -32,15 +32,17 @@ if {$nparams < 5} {
 set selector [lindex $argv 0]
 set swchar   [string index $selector 0]
 
+
 if {$swchar != "-"} {
-    exec $cesbcnaf $argv
+    catch {exec $cesbcnaf $argv 2>/dev/null} msg
 } else {
     if {$selector == "-ces"} {
-	exec $cesbcnaf [lrange $argv 1 end]
+	catch {exec $cesbcnaf [lrange $argv 1 end] 2>/dev/null} msg
     } elseif {$selector == "-wiener"} {
-	exec $wienerbcnaf [lrange $argv 1 end]
+	catch {exec $wienerbcnaf [lrange $argv 1 end] 2>/dev/null} msg
     } else {
 	usage
 	exit -1
     }
 }
+puts $msg
