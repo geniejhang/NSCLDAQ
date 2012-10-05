8f119c6844443854edb76d42cfe7f1b721263f08
Ron Fox <fox@nscl.msu.edu>
2012-10-05 20:59:43 +0000

    add EVB::outputSummary widget as well as internal test methods.

--- daq/eventbuilder/outputStatistics.tcl
+++ daq/eventbuilder/outputStatistics.tcl
@@ -37,6 +37,66 @@ namespace eval EVB {
 #-----------------------------------------------------------------------------------
 
 ##
+# @class EVB::outputSummary
+#
+#  Widget that summarizes the output statistics of the event orderer.
+#
+# OPTIONS:
+#  - -fragments        - Number of fragments that have been output
+#  - -hottestid        - Source id with the most fragments output.
+#  - -hottestcount     - Number of fragments output by -hottestid.
+#  - -coldestid        - Source id with the fewest output fragments.
+#  - -coldestcount     - Number of fragments output by -coldestid.
+#
+#  LAYOUT:
+#   Rough layout.
+# \verbatim
+#
+#   +-----------------------------------------+
+#   |Total Fragments          <fragcount>     |
+#   |           id            count           |
+#   | hottest   <id>          <fragcount>     |
+#   | coldest   <id>          <fragcount>     |
+#   +-----------------------------------------+
+# \endverbatim
+#
+snit::widget ::EVB::outputSummary {
+    option -fragments    -default 0
+    option -hottestid    -default ""
+    option -hottestcount -default ""
+    option -coldestid    -default ""
+    option -coldestcount -default ""
+    
+    
+    constructor args {
+        #  Create the widgets (all ttk::label s)
+        
+        ttk::label $win.fragl   -text {Total fragments}
+        ttk::label $win.idl     -text {source id}
+        ttk::label $win.countl  -text {fragment count}
+        ttk::label $win.hotl    -text {Hottest}
+        ttk::label $win.coldl   -text {Coldest}
+        
+        ttk::label $win.frag           -textvariable ${selfns}::options(-fragments)
+        ttk::label $win.hotid          -textvariable ${selfns}::options(-hottestid)
+        ttk::label $win.hotcount       -textvariable ${selfns}::options(-hottestcount)
+        ttk::label $win.coldid         -textvariable ${selfns}::options(-coldestid)
+        ttk::label $win.coldcount      -textvariable ${selfns}::options(-coldestcount)
+        
+        # Lay them out
+        
+        grid $win.fragl   -           $win.frag
+        grid x            $win.idl    $win.countl
+        grid $win.hotl    $win.hotid  $win.hotcount  -sticky e
+        grid $win.coldl   $win.coldid $win.coldcount -sticky e
+        
+        # Process the initial configuration.
+        
+        $self configurelist $args
+    }
+}
+
+##
 # @class EVB::outputStatistics
 #
 #   This snit::widget provides a display of the output statistics for the event
@@ -260,4 +320,69 @@ snit::widget ::EVB::outputStatistics {
     }
 
 
+}
+
+#----------------------------------------------------------------------------
+#
+# internal self test methods.
+
+namespace eval EVB {
+    namespace eval test {}
+}
+##
+# test the outputSummary widget:
+#
+proc ::EVB::test::outputSummary {} {
+    ::EVB::outputSummary .target
+    pack .target
+    
+    # now the control panel:
+    
+    toplevel .control
+    
+    label .control.fragl -text Fragments
+    entry .control.frag  -textvariable -fragments
+    
+    label .control.hotidl -text {Hot id}
+    entry .control.hotid  -textvariable -hottestid
+    
+    label .control.hotcountl -text {Hot count}
+    entry .control.hotcount  -textvariable -hottestcount
+    
+    label .control.coldidl  -text {Cold id}
+    entry .control.coldid   -textvariable -coldestid
+    
+    label .control.coldcountl -text {Cold Count}
+    entry .control.coldcount -textvariable -coldestcount
+    
+    grid .control.fragl .control.frag
+    grid .control.hotidl .control.hotid
+    
+    grid .control.hotcountl .control.hotcount
+    
+    grid .control.coldidl .control.coldid
+    grid .control.coldcountl .control.coldcount
+
+    # Add variable traces to propagate values -> the .target.
+    
+    foreach var [list -fragments -hottestid -hottestcount -coldestid -coldestcount] {
+        trace add variable ::$var write [list ::EVB::test::updateWidgetOption .target]
+    }
+}
+##
+# Testing utility to update a widget option from a trace on a variable
+# that has a name matching the option:
+#
+# @param widget - the widget to modify.
+# @param name1  - name of the variable (and option)
+# @param name2  - index of the variable if an array.
+# @param op     - operation performed.
+#
+
+if {[info proc ::EVB::test::updateWidgetOption] eq ""} {
+    proc ::EVB::test::updateWidgetOption {widget name1 name2 op} {
+        upvar #0 $name1 value
+        $widget configure $name1 $value
+        return ""
+    }
 }
