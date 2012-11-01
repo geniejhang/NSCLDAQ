112c3f8e4c952b83e708fe3ba9686e9b8e861e02
Ron Fox <fox@nscl.msu.edu>
2012-11-01 20:15:34 +0000

    Late fragment megawidget.

--- daq/eventbuilder/late.tcl
+++ daq/eventbuilder/late.tcl
@@ -13,6 +13,13 @@
 #	     East Lansing, MI 48824-1321
 
 
+# add this directory to auto_path if it's not there yet.
+
+set here [file dirname [info script]]
+if {[lsearch $::auto_path $here] == -1} {
+    lappend ::auto_path $here
+}
+
 ##
 # @file late.tcl
 # @brief Provide data late GUI information.
@@ -22,71 +29,10 @@
 package provide Late 1.0
 package require Tk
 package require snit
+package require EVBUtilities
 
 namespace eval EVB {}
 
-##
-# @class EVB::sourceLate 
-#
-# Provides a widget that gives information about the
-# data late conditions for a specific data source.
-# 
-# OPTIONS
-#    - -id  - Source id of the widget.
-#    - -count - Number of data late conditions.
-#    - -worst - Delta t of worst case late.
-#
-# FORMAT
-#
-#  +---------------------------------------+
-#  | Sourceid    #lates       #worstDt     |
-#  +---------------------------------------+
-
-snit::widget EVB::late {
-    component innerHull
-
-    option -id
-    option -count
-    option -worst
-
-    delegate option * to innerHull
-
-    ##
-    # constructor.
-    #  - Create/layout widgets.
-    #  - process options.
-    #
-    # @param args Argument value parirs.
-    #
-    constructor args {
-    
-	install innerHull using ttk::frame $win.innerHull 
-
-	ttk::label $win.innerHull.sourceid \
-	    -textvariable ${selfns}::options(-id) \
-	    -width 5
-	ttk::label $win.innerHull.count \
-	    -textvariable ${selfns}::options(-count)  \
-	    -width 6
-	ttk::label $win.innerHull.worst \
-	    -textvariable ${selfns}::options(-worst) \
-	    -width 11
-
-	# layout the widgets in the inner hull:
-
-	grid $win.innerHull.sourceid \
-	     $win.innerHull.count    \
-	     $win.innerHull.worst -sticky w
-
-	# Make the inner hull fill the actual hull:
-
-	grid $win.innerHull -sticky nsew
-
-
-	$self configurelist $args
-    }
-   
-}
 
 ##
 # @class EVB::lateSummary
@@ -150,6 +96,42 @@ snit::widget EVB::lateSummary {
     }
 }
 ##
+# EVB::labelpair
+#
+#   A pair of labels glued into a single widget (see lateFragments below
+#   for one use case).
+#
+#   +----------------------------+
+#   | leftlabel      | rightlabel|
+#   +----------------------------+
+#
+# OPTIONS
+#   -lefttext   - text in left label.
+#   -righttext  - text in right label.
+#
+snit::widgetadaptor ::EVB::labelpair {
+    component left
+    component right
+    
+    delegate option -lefttext to left   as -text
+    delegate option -righttext to right as -text
+    
+    ##
+    # Construct this all inside a ttk::frame hull:
+    #
+    constructor args {
+        installhull using ttk::frame
+        
+        install left  using ttk::label $win.left -width 10
+        install right using ttk::label $win.right -width 10
+        
+        grid $left $right
+        
+        $self configurelist $args
+    }
+}
+
+##
 # EVB::lateFragments
 #
 #  Displays the late fragment statistics.
@@ -183,15 +165,78 @@ snit::widget EVB::lateSummary {
 #   # The individual source data includes a 
 #   scroll bar if needed.
 #
-snit::widget EVB::lateFragments {
-    component innerHull
+snit::widgetadaptor EVB::lateFragments {
     component summary
-
+    component container
+    
     delegate option -count to summary
     delegate option -worst to summary
-    delegate option * to innerHull
-
-
-
+    
+    delegate method clear to container as reset
+    
+    ##
+    # constructor
+    #    install the ttk::frame as a hull.
+    #    install the containers as a sortedWidget
+    #    Lay all this out appropriately.
+    #
+    # @param args - configuration option values.
+    #
+    constructor args {
+        installhull using ttk::frame
+        
+        install summary using ::EVB::lateSummary $win.summary
+        install container using ::EVB::utility::sortedWidget $win.container \
+            -title {Per source} -lefttitle {Id count} -righttitle {worst} \
+            -create [mymethod _CreateSource] -update [mymethod _UpdateSource]
+        
+        grid $summary -sticky new
+        grid $container -sticky sew
+        
+        $self configurelist $args
+    }
+    ##
+    # source id count late
+    #
+    #   Sets the count and worst case time difference for a source.
+    #   If needed this makes a new widget.
+    #   This will always generate a callback to _UpdateSource and, if the
+    #   source does not exist will generate a callback to _CreateSource
+    #
+    # @param id    - Source id.
+    # @param count - Number of late packets
+    # @param worst - time difference of worst case late packet.
+    #
+    method source {id count worst} {
+        $container update $id [list $count $worst]
+    }
+    #-----------------------------------------------------------------------
+    #
+    #  Private methods.
+    
+    ##
+    # _CreateSource widget
+    #
+    #  Creates a widget for the statistics for an event source.  This is
+    #  an ::EVB::labelpair
+    #
+    # @param path - the widget name path desired.
+    #
+    method _CreateSource widget {
+        ::EVB::labelpair $widget
+    }
+    ##
+    #  UpdateSource widget data
+    #    Updates the widget corresponding to a data source.
+    #
+    # @param widget - name path of the widget to update.
+    # @param data   - Pair containing in turn the late count and worst case time
+    #                 difference.
+    #
+    method _UpdateSource {widget data} {
+        set count [lindex $data 0]
+        set worst [lindex $data 1]
+        $widget configure -lefttext $count -righttext $worst
+    }
 }
 
