1036f29c3d37c0c2716498558efb15d324f5e80b
Ron Fox <fox@nscl.msu.edu>
2014-12-23 12:24:05 +0000

    Add the marker creator files

---
+++ usb/vmusb/tclserver/CMarkerCreator.h
@@ -0,0 +1,39 @@
+/**
+
+#    This software is Copyright by the Board of Trustees of Michigan
+#    State University (c) Copyright 2013.
+#
+#    You may use this software under the terms of the GNU public license
+#    (GPL).  The terms of this license are described at:
+#
+#     http://www.gnu.org/licenses/gpl.txt
+#
+#    Author:
+#            Ron Fox
+#            NSCL
+#            Michigan State University
+#            East Lansing, MI 48824-1321
+
+##
+# @file   CMarkerCreator.h
+# @brief  Creational class for CMarkerObject objects.
+# @author <fox@nscl.msu.edu>
+*/
+
+#ifndef _CMARKERCREATOR_H
+#define _CMARKERCREATOR_H
+
+#include "CModuleCreator.h"
+
+/**
+ * @class CMarkerCreator
+ *     Creates CMarkerObject items.
+ */
+class CMarkerCreator : public CModuleCreator
+{
+public:
+    virtual CControlHardware* operator()(std::string name);
+};
+
+
+#endif
