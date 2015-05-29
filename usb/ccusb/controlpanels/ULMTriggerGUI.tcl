3afab29c9d852622e9c5950c639f0b252fc1b78c
Jeromy Tompkins <tompkins@nscl.msu.edu>
2015-05-29 17:35:41 +0000

    Fixes some issues with config files in ULMTriggerGUI

--- usb/ccusb/controlpanels/ULMTriggerGUI.tcl
+++ usb/ccusb/controlpanels/ULMTriggerGUI.tcl
@@ -25,7 +25,7 @@ set options {
   {-host.arg "localhost"  "host running VMUSBReadout slow-controls server" }
   {-port.arg  27000       "port the slow-controls server is listening on" }
   {-ring.arg $::tcl_platform(user) "name of ring VMUSBReadout is filling" }
-  {-configfile.arg "ulmtrigger.tcl"  "name of file to write GUI state to" }
+  {-configfile.arg ""  "name of file to write GUI state to" }
 }
 
 set usage " --module value ?option value? :"
@@ -64,12 +64,8 @@ set path [lindex [array get params -configfile] 1]
 
 
 ATrigger2367 mygui 0 1 $host $port $module $slot 
+mygui SetupGUI . $path
 
-if {$path ne ""} {
-  mygui SetupGUI . $path
-} else {
-  tk_messageBox -icon error -message "User must supply a valid ---configfile argument"
-}
 # Configure some window manager details 
 wm protocol . WM_DELETE_WINDOW { mygui OnExit ; destroy . } 
 wm resizable . false false
