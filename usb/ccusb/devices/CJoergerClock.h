f4896794bab985c41e2ad70ee4a7a545b843f40f
Ron Fox <fox@nscl.msu.edu>
2016-11-15 18:44:07 -0500

    Placeholder for joerger clock driver header.

---
+++ usb/ccusb/devices/CJoergerClock.h
@@ -0,0 +1,57 @@
+/*******************************************************************************
+*
+* CAEN SpA - System Integration  Division
+* Via Vetraia, 11 - 55049 - Viareggio ITALY
+* +390594388398 - www.caen.it
+*
+  
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox
+	
+     @file CJoergerClock.h
+     @brief Define driver for the Joerger clock module.
+*/
+
+#ifndef CJOERGERCLOCK_H
+#define CJOERGERCLOCK_H
+
+#ifndef __CREADOUTHARDWARE_H
+#include "CReadoutHardware.h"
+#endif
+
+#ifndef __CRT_STDINT_H
+#include <stdint.h>
+#ifndef __CRT_STDINT_H
+#define __CRT_STDINT_H
+#endif
+#endif
+
+#ifndef __STL_STRING
+#include <string>
+#ifndef __STL_STRING
+#define __STL_STRING
+#endif
+#endif
+
+#ifndef __STL_VECTOR
+#include <vector>
+#ifndef __STL_VECTOR
+#define __STL_VECTOR
+#endif
+#endif
+
+
+// Forward class definitions:
+
+class CReadoutModule;
+class CCCUSB;
+class CCCUSBReadoutList;
+
+
+
+#endif
