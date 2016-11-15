79a78c8591f07de6713f86c1ee14ae76b0fe7987
Ron Fox <fox@nscl.msu.edu>
2016-11-15 18:52:00 -0500

    FInished coding the header

--- usb/ccusb/devices/CJoergerClock.h
+++ usb/ccusb/devices/CJoergerClock.h
@@ -52,6 +52,38 @@ class CReadoutModule;
 class CCCUSB;
 class CCCUSBReadoutList;
 
+/**
+ *  @class CJoergerClock
+ *    Clock module based on the sketchy info I have from Cary? at UTK.edu.
+ *    The module has 4 outputs and the clock rate is programmable for each
+ *    output, by writing F17@An for each output.  It's not so clear what
+ *    the values mean, therefore we'll just expose the raw values.
+ *    Configuration parameters:
+ *    - -slot - crate slot number.
+ *    - -chan0 - Value to program in chan0.
+ *    - -chan1 - Value to program in chan1.
+ *    - -chan2 - Value to program in chan2.
+ *    - -chan3 - Value to program in chan3.
+ */
+class CJoergerClock : public CReadoutHardware
+{
+public:
+  CJoergerClock();
+  CJoergerClock(const CJoergerClock& rhs);
+  virtual ~CJoergerClock();
+private:
+  int operator==(const CJoergerClock& rhs) const;
+  int operator!=(const CJoergerClock& rhs) const;
 
+  // The interface CReadoutHardware derived objects must 
+  // implement.
+
+
+public:
+  virtual void onAttach(CReadoutModule& configuration);
+  virtual void Initialize(CCCUSB& controller);
+  virtual void addReadoutList(CCCUSBReadoutList& list);
+  virtual CReadoutHardware* clone() const;
+};
 
 #endif
