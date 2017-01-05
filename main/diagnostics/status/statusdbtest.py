67a11c87d31cf9f18c93a00460ecd8fe35548b55
Ron Fox <fox@nscl.msu.edu>
2017-01-05 08:27:39 -0500

    *  Correctly get the setup/teardown for status db tests right *  Test the addLogMessage method of a status database object. This both    proves the addLogMessage method and proves the setup/teardown.

--- main/diagnostics/status/statusdbtest.py
+++ main/diagnostics/status/statusdbtest.py
@@ -22,10 +22,42 @@
 # @author <fox@nscl.msu.edu>
 
 import unittest
+import sqlite3
+import tempfile
+
 from nscldaq.status import statusdb
+from nscldaq.status  import statusmessages
 
 class StatusDbTests(unittest.TestCase):
-    pass
+    def setUp(self):
+        dbfile       = tempfile.NamedTemporaryFile()
+        self._dbname  = dbfile.name
+        dbfile.close()
+        self._api    = statusdb.statusdb(self._dbname)
+        self._sqlite = sqlite3.connect(self._dbname)
+        self._sqlite.row_factory = sqlite3.Row
+        
+    def teardown(self):
+        self._sqlite.close()
+        self._api = None                   # Should force destruction via GC.
+        self._rowFact = None
+        
+    def test_addLogMessage(self):
+        self._api.addLogMessage(
+            statusmessages.SeverityLevels.WARNING, 'some-application',
+            'charlie.nscl.msu.edu', 1234, 'Something to talk about'
+        )
+        #  Should be an appropriate entry in 
+        
+        c = self._sqlite.cursor()
+        c.execute('SELECT * FROM  log_messages')
+        r = c.fetchone()
+        self.assertEqual(1, r['id'])
+        self.assertEqual('WARNING', r['severity'])
+        self.assertEqual('some-application', r['application'])
+        self.assertEqual('charlie.nscl.msu.edu', r['source'])
+        self.assertEqual(1234, r['timestamp'])
+        self.assertEqual('Something to talk about', r['message'])
 
 
 
