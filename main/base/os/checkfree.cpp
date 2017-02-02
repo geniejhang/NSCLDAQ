afe7a831558d8938835d094126f6a8f2f7a64882
Ron Fox <fox@nscl.msu.edu>
2017-02-02 15:24:20 -0500

    Draft of program to check io::freeSpacePercent

---
+++ main/base/os/checkfree.cpp
@@ -0,0 +1,38 @@
+/**
+
+#    This software is Copyright by the Board of Trustees of Michigan
+#    State University (c) Copyright 2013.
+#
+#    You may use this software under the terms of the GNU public license
+#    (GPL).  The terms of this license are described at:
+#
+#     http://www.gnu.org/licenses/gpl.txt
+#
+#    Author:
+#            Ron Fox
+#            NSCL
+#            Michigan State University
+#            East Lansing, MI 48824-1321
+
+##
+# @file   checkfree.cpp
+# @brief  Display the pct of free space on a filesystem given a filename.
+# @author <fox@nscl.msu.edu>
+*/
+
+#include <os.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char**argv)
+{
+    const char *fname = argv[1];
+    int         fd    = open(fname, O_RDONLY);
+    
+    double result = io::freeSpacepercent(fd);
+    
+    close(fd);
+    
+    std::cout << result << " percent\n";
+}
