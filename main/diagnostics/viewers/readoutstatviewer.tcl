5a2f827dc6f8a1970977d0ad18f052e1652a766a
Ron Fox <fox@nscl.msu.edu>
2017-02-17 08:45:43 -0500

    Make readoutsatviewer a bit more productiony.

--- main/diagnostics/viewers/readoutstatviewer.tcl
+++ main/diagnostics/viewers/readoutstatviewer.tcl
@@ -1,8 +1,48 @@
+#!/bin/sh
+# -*- tcl -*-
+# The next line is executed by /bin/sh, but not tcl \
+exec tclsh "$0" ${1+"$@"}
+
+#    This software is Copyright by the Board of Trustees of Michigan
+#    State University (c) Copyright 2014.
+#
+#    You may use this software under the terms of the GNU public license
+#    (GPL).  The terms of this license are described at:
+#
+#     http://www.gnu.org/licenses/gpl.txt
+#
+#    Authors:
+#             Ron Fox
+#             Jeromy Tompkins 
+#	     NSCL
+#	     Michigan State University
+#	     East Lansing, MI 48824-1321
+
+set here [file dirname [info script]]
+set libdir [file normalize [file join $here .. TclLibs]]
+lappend auto_path $libdir
+
+##
+# @file readoutstatviewer.tcl
+# @brief Status display application for readout statistics.
+# @author Ron Fox <fox@nscl.msu.edu>
+#
 package require ReadoutStatModel
 package require ReadoutStatView
 package require ReadoutStatController
 package require statusMessage
 
+
+if {[llength $argv] != 1} {
+    puts stderr "Usage:"
+    puts stderr "    readoutstatsviewer dbfile"
+    puts stderr "Where:"
+    puts stderr "    dbfile - is the database file with the statistics information"
+    exit -1
+}
+
+
+# Glue together the MVC and then we're done.
 
 set db [statusdb create $argv readonly]
 
