c561ed5c11754b76f5828d3bd4d2e31c4cacf752
Jeromy Tompkins <tompkins@nscl.msu.edu>
2017-03-01 09:25:14 -0500

    Adds CProcessCountPredicate

--- main/utilities/conversion/CProcessCountPredicate.cpp
+++ main/utilities/conversion/CProcessCountPredicate.cpp
@@ -15,15 +15,106 @@
 */
 
 #include <CProcessCountPredicate.h>
-#include <CRunStatistics.h>
+
+namespace DAQ {
+namespace Transform {
 
 
-CProcessCountPredicate::CProcessCountPredicate(size_t nToProcess, CRunStatistics& stats)
-  : m_stats(stats), m_toProcess(nToProcess)
+CProcessCountPredicate::CProcessCountPredicate(size_t nToSkip, size_t nToProcess)
+  : m_toSkip(nToSkip),
+    m_toProcess(nToProcess),
+    m_skipped(0),
+    m_processed(0)
 {}
 
 
-bool CProcessCountPredicate::operator()()
+CPredicatedMediator::Action
+CProcessCountPredicate::preInputUpdate(CPredicatedMediator &transform)
 {
-  return (m_stats.getProcessCount() < m_toProcess);
+    return CPredicatedMediator::CONTINUE;
 }
+
+
+CPredicatedMediator::Action
+CProcessCountPredicate::postInputUpdate(CPredicatedMediator &transform, int type)
+{
+    if (m_skipped < m_toSkip) {
+        m_skipped++;
+        return CPredicatedMediator::SKIP;
+    }
+
+    if (m_processed < m_toProcess) {
+        m_processed++;
+        return CPredicatedMediator::CONTINUE;
+    } else {
+        return CPredicatedMediator::ABORT;
+    }
+}
+
+
+CPredicatedMediator::Action
+CProcessCountPredicate::preOutputUpdate(CPredicatedMediator &transform, int type)
+{
+    return CPredicatedMediator::CONTINUE;
+}
+
+CPredicatedMediator::Action
+CProcessCountPredicate::postOutputUpdate(CPredicatedMediator &transform, int type)
+{
+    return CPredicatedMediator::CONTINUE;
+}
+
+void CProcessCountPredicate::reset()
+{
+    m_skipped = 0;
+    m_processed = 0;
+}
+
+void CProcessCountPredicate::setSkipCount(size_t count)
+{
+    m_skipped = count;
+}
+
+size_t CProcessCountPredicate::getSkipCount() const
+{
+    return m_skipped;
+}
+
+
+
+void CProcessCountPredicate::setNumberToSkip(size_t nToSkip)
+{
+    m_toSkip = nToSkip;
+}
+
+size_t CProcessCountPredicate::getNumberToSkip() const
+{
+    return m_toSkip;
+}
+
+
+
+void CProcessCountPredicate::setProcessCount(size_t count)
+{
+    m_processed = count;
+}
+
+size_t CProcessCountPredicate::getProcessCount() const
+{
+    return m_processed;
+}
+
+
+void CProcessCountPredicate::setNumberToProcess(size_t nToProcess)
+{
+    m_toProcess = nToProcess;
+}
+
+size_t CProcessCountPredicate::getNumberToProcess() const
+{
+    return m_toProcess;
+}
+
+
+} // end Transform
+} // end DAQ
--- main/utilities/conversion/CProcessCountPredicate.h
+++ main/utilities/conversion/CProcessCountPredicate.h
@@ -19,17 +19,56 @@
 
 #include <CPredicate.h>
 
-class CRunStatistics;
+namespace DAQ {
+namespace Transform {
 
 class CProcessCountPredicate: public CPredicate
 {
   private:
-    CRunStatistics& m_stats;
+    size_t m_toSkip;
     size_t m_toProcess;
-  public:
-    CProcessCountPredicate(size_t nToProcess, CRunStatistics& stats);
+    size_t m_skipped;
+    size_t m_processed;
 
-    bool operator()();
+  public:
+    CProcessCountPredicate(size_t nToSkip, size_t nToProcess);
+    CProcessCountPredicate(const CProcessCountPredicate& ) = default;
+    CProcessCountPredicate& operator=(const CProcessCountPredicate& ) = default;
+
+    virtual CPredicatedMediator::Action
+    preInputUpdate(CPredicatedMediator& transform);
+
+    virtual CPredicatedMediator::Action
+    postInputUpdate(CPredicatedMediator& transform,
+                    int type);
+
+    virtual CPredicatedMediator::Action
+    preOutputUpdate(CPredicatedMediator& transform,
+                    int type);
+
+    virtual CPredicatedMediator::Action
+    postOutputUpdate(CPredicatedMediator& transform,
+                     int type);
+
+    virtual void reset();
+
+    void setNumberToSkip(size_t nToSkip);
+    size_t getNumberToSkip() const;
+
+    void setSkipCount(size_t count);
+    size_t getSkipCount() const;
+
+    void setNumberToProcess(size_t nToProcess);
+    size_t getNumberToProcess() const;
+
+    void setProcessCount(size_t count);
+    size_t getProcessCount() const;
+
+
 };
+
+
+} // end Transform
+} // end DAQ
 
 #endif
