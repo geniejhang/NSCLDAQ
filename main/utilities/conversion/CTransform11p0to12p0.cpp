b1222297982758f3ac6549103f49ac05edc6fc81
Jeromy Tompkins <tompkins@nscl.msu.edu>
2017-03-24 15:58:35 -0400

    Adds some new files for 11.0 -> 12.0

---
+++ main/utilities/conversion/CTransform11p0to12p0.cpp
@@ -0,0 +1,5 @@
+#include "CTransform11p0to12p0.h"
+
+CTransform11p0to12p0::CTransform11p0to12p0()
+{
+}
---
+++ main/utilities/conversion/CTransform11p0to12p0.h
@@ -0,0 +1,10 @@
+#ifndef CTRANSFORM11P0TO12P0_H
+#define CTRANSFORM11P0TO12P0_H
+
+class CTransform11p0to12p0
+{
+public:
+    CTransform11p0to12p0();
+};
+
+#endif // CTRANSFORM11P0TO12P0_H
