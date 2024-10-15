3669867f601956d379bb26f95cbfad6f17251ff7
giordano cerizza <cerizza@nscl.msu.edu>
2024-10-15 06:58:54 -0400

    srs - started adaptation for firmware with hit markers

--- main/srs/broker/UDPBrokerDerived.h
+++ main/srs/broker/UDPBrokerDerived.h
@@ -45,7 +45,7 @@ protected:
 private:
 
     void makeRingItems(in_addr_t from, short port, CDataSink& sink, int sid, uint8_t* buffer, size_t nBytes);
-    int extractHitTimeStamp(uint8_t sourceId, uint8_t* data);
+    int getExtraData(uint8_t sourceId, uint8_t* data);
     void mapping(uint8_t* data, int fecId);
     void testReadData(uint8_t* data);
     uint16_t invertByteOrder(uint16_t data);
@@ -62,7 +62,9 @@ private:
     static const int MaxFECs{16};
     static const int MaxVMMs{32};//MaxVMMs used for markers, 16 normal trigger + 16 ext trigger
     static const int MaxChns{64};
-    bool startedMarker[MaxFECs * MaxVMMs] = {false};
+    bool m_startedMarker[MaxFECs * MaxVMMs] = {false};
+    bool m_dataEnded = true;
+
 
     int m_hitCounter;
     int m_datagramCounter;
@@ -76,17 +78,16 @@ private:
     struct VMM3Marker
     {
       uint64_t fecTimeStamp{0};  /// 42 bit
-      uint64_t calcTimeStamp{0}; /// 42 bit
-      uint16_t lastTriggerOffset{0};
-      bool hasDataMarker{false};
+      uint64_t hitMarker{0}; /// 42 bit
     } *markerSRS;
 
     struct newData
     {
         uint64_t hitTimeStamp{0};
         uint64_t hitContinuousMarker{0};
+        uint64_t hitMarker{0};
         uint16_t chnoMapped{0};
-    } tsMarkersAndMappedChno;
+    } extraData;
 
     static const int SRSHeaderSize{16};
     static const int HitAndMarkerSize{6};
