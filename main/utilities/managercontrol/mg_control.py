03dda7c5930f06d28ad664536673af2c72521241
Ron Fox <fox@frib.msu.edu>
2024-11-11 09:04:10 -0500

    Issue #218 - Improve manager control panel.
    Got aggregate output widget working.

--- main/utilities/managercontrol/mg_control.py
+++ main/utilities/managercontrol/mg_control.py
@@ -7,7 +7,7 @@ from nscldaq.manager_control.config import Configuration
 from nscldaq.manager_control.programlist import ProgramView
 from nscldaq.manager_control.cfgwizard import ConfigWizard
 from nscldaq.manager_control.maingui import MainGui
-from nscldaq.manager_client import Programs
+from nscldaq.manager_client import Programs, OutputMonitor
 from PyQt5.QtWidgets import (
     QApplication, QLabel, QLineEdit,
     QMainWindow
@@ -21,6 +21,7 @@ CONFIGURATION_FILE="mg_guiconfig.toml"
 UPDATE_INTERVAL=2        # Units are sedonds
 
 Updater = None          # Early in initialization this becomes a QTimer. Updaters hook to the timeout signal.
+outputMonitor = None
 
 
 
@@ -37,6 +38,8 @@ def get_configuration():
         c.dump(CONFIGURATION_FILE)
         return c
 
+#----------------------------------------------------------------------------------------------------
+#  Stuff for the programs display.
 
 def addProgramDisplay(window):
     programList = ProgramView()
@@ -46,9 +49,41 @@ def addProgramDisplay(window):
     
 
 def updateProgramsTab():
+    # Timeout handler to update the model driving the programs tab.
+    
     programs = Programs(config.host(), config.user(), config.rest_service())
-    print('updating')
+
     programlist.model().update(programs.status())
+
+
+#-------------------------------------------------------------------- 
+#  Handle output logging.
+
+def updateLogWindows():
+    # called periodically to check for output. 
+    
+    # Reconnect if we can:
+    
+    if not  outputMonitor.isConnected():
+        try:
+            outputMonitor.reconnect()
+        except:
+            return     # reconnect failed this time.
+    
+    # continue if we're still connected:
+    
+    if outputMonitor.isConnected():
+        full_output = ""
+        output = outputMonitor.read()
+        while len(output) > 0:
+            full_output += output    # Aggregate the otputut.
+            output = outputMonitor.read()
+        # Now we have the full_output:
+        # Put it all in aggregated output:
+        
+        if len(full_output) > 0:
+            
+            gui.log().log('Aggregate output', full_output)
 
 #---------------------------------- Entry point -----------------------
 #
@@ -68,13 +103,21 @@ config = get_configuration()
 # Now the GUI:
 
 main_window = QMainWindow()
-
 gui = MainGui()
+
+#  Add the dynamic program list:
 programlist = addProgramDisplay(gui)
 filter = config.readouts()
-print("filtering to ", filter)
 programlist.model().setFilter(config.readouts())
 Updater.timeout.connect(updateProgramsTab)
+
+# Set up output logging:
+
+outputMonitor = OutputMonitor(config.host(), config.user(), config.monitor_service())
+Updater.timeout.connect(updateLogWindows)
+
+
+
 main_window.setCentralWidget(gui)
 
 main_window.show()
